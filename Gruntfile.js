67dc8dee8ed517706f5826db0048149ae751f12c
Justin Heideman <justin.heideman@nytimes.com>
2015-12-22 16:44:24 -0500

    getting less set up

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,34 @@
 'use strict';
 
 module.exports = function (grunt) {
+
+  // Load npm plugins to provide necessary tasks.
+  grunt.loadNpmTasks('assemble');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+
+
   // Project configuration.
   grunt.initConfig({
 
     clean: {
       example: ['dist/*']
+    },
+
+    less: {
+      main: {
+        options: {
+            sourceMap: true,
+            relativeUrls: false,
+            compress: false,
+            paths: ['src/assets/css']
+        },
+        files: {
+          'dist/assets/css/main.css': 'src/less/main.less'
+        }
+      }
     },
 
     copy: {
@@ -36,12 +59,6 @@ module.exports = function (grunt) {
     }
   });
 
-  // Load npm plugins to provide necessary tasks.
-  grunt.loadNpmTasks('assemble');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-
-
   // Default tasks to be run.
-  grunt.registerTask('default', ['clean', 'copy', 'assemble']);
+  grunt.registerTask('default', ['clean', 'copy', 'less', 'assemble']);
 };
