ea5b552882f6674525346329b670f3d37b721c5d
Justin Heideman <justin.heideman@nytimes.com>
2016-01-02 18:42:44 -0500

    cleanup for pages

---
+++ README.md
@@ -0,0 +1,20 @@
+# Goldstein Museum of Design Collections Template
+
+## Installation
+
+This assumes you have git and node/npm installed.
+
+1. Clone this repo
+2. run `npm install`
+
+## Building
+
+1. Run `grunt`
+
+Output will be located in the `dist/` directory.
+
+## Development
+
+1. Make sure that `npm install` has been run
+2. Install the [LiveReload browser extension](https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei)
+3. Run `grunt watch`
---
+++ src/js/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+(function (){
+
+    var $object_detail = $('.object-detail');
+    var $oDLink = $('#object-detail-image-link');
+    var $oDImg = $('#object-detail-image-link img');
+
+    //object detail page
+    if ($object_detail.length > 0){
+        //swap images on click/tap
+        $('.object-detail').find('.object-images-list a').click(function (e){
+            var $this = $(this);
+            var medUrl = $this.data('med-url') ? $this.data('med-url') : $this.attr('href');
+            e.preventDefault();
+            $oDLink.attr('href', $this.attr('href'));
+            $oDImg.addClass('loading').one('load', function () {
+                $(this).removeClass('loading');
+            }).attr('src', medUrl);
+        });
+    }
+
+})();
